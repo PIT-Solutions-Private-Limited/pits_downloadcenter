0be3f4d35a8c1e42dc049becd340e630eeff80b1
Hoja M A <hoja.ma@pitsolutions.com>
2015-10-01 11:54:38 +0530

    Minor Tweaks

--- Classes/Controller/DownloadController.php
+++ Classes/Controller/DownloadController.php
@@ -173,112 +173,117 @@ class DownloadController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControl
     public function forceDownloadAction(){
         $arguments          = $this->request->getArguments();
         $fileID             = $arguments['fileid'];
-        $storageuid         = $this->settings['fileStorage'];
-        $fileDetails        = $this->downloadRepository->getFileDetails( $storageuid , $fileID  );
-        $fileIdentifier     =  (isset($fileDetails['identifier']))?$fileDetails['identifier']:FALSE;
-        $storageRepository  = $this->storageRepository->findByUid( $storageuid );
-        $sConfig            = $storageRepository->getConfiguration();
-        $fileName           = (isset($fileDetails['name']))?$fileDetails['name']:NULL;
-        $file               =  realpath( PATH_site.$sConfig['basePath'].$fileIdentifier ); 
-        $fileObject 		= $storageRepository->getFile($fileIdentifier);
-        $sys_language_uid	= $GLOBALS['TSFE']->sys_language_uid;
-        $checkTranslations = $this->downloadRepository->checkTranslations( $fileObject , $sys_language_uid );
-        if( $checkTranslations ):
-       		$file_identifier = isset($checkTranslations['translated_file'])?$checkTranslations['translated_file']:NULL;
-        	if ($file_identifier && !empty( $file_identifier )){
-        		$filepath = PATH_site.$file_identifier;
-        		if (!empty($filepath) && is_file($filepath)){
-        			$file = $filepath;
-        			$fileName = basename( $file );
-        		}
-        	}
-        endif;
-        if(is_file($file)) {
-            $fileLen    = filesize($file);          
-            $ext        = strtolower(substr(strrchr($fileName, '.'), 1));
-            switch($ext) {
-                case 'txt':
-                    $cType = 'text/plain'; 
-                break;              
-                case 'pdf':
-                    $cType = 'application/pdf'; 
-                break;
-                case 'exe':
-                    $cType = 'application/octet-stream';
-                break;
-                case 'zip':
-                    $cType = 'application/zip';
-                break;
-                case 'doc':
-                    $cType = 'application/msword';
-                break;
-                case 'xls':
-                    $cType = 'application/vnd.ms-excel';
-                break;
-                case 'ppt':
-                    $cType = 'application/vnd.ms-powerpoint';
-                break;
-                case 'gif':
-                    $cType = 'image/gif';
-                break;
-                case 'png':
-                    $cType = 'image/png';
-                break;
-                case 'jpeg':
-                case 'jpg':
-                    $cType = 'image/jpg';
-                break;
-                case 'mp3':
-                    $cType = 'audio/mpeg';
-                break;
-                case 'wav':
-                    $cType = 'audio/x-wav';
-                break;
-                case 'mpeg':
-                case 'mpg':
-                case 'mpe':
-                    $cType = 'video/mpeg';
-                break;
-                case 'mov':
-                    $cType = 'video/quicktime';
-                break;
-                case 'avi':
-                    $cType = 'video/x-msvideo';
-                break;
+        if( is_numeric($fileID)) {
+			$storageuid         = $this->settings['fileStorage'];
+			$fileDetails        = $this->downloadRepository->getFileDetails( $storageuid , $fileID  );
+			$fileIdentifier     =  (isset($fileDetails['identifier']))?$fileDetails['identifier']:FALSE;
+			$storageRepository  = $this->storageRepository->findByUid( $storageuid );
+			$sConfig            = $storageRepository->getConfiguration();
+			$fileName           = (isset($fileDetails['name']))?$fileDetails['name']:NULL;
+			$file               =  realpath( PATH_site.$sConfig['basePath'].$fileIdentifier ); 
+			$fileObject 		= $storageRepository->getFile($fileIdentifier);
+			$sys_language_uid	= $GLOBALS['TSFE']->sys_language_uid;
+			$checkTranslations = $this->downloadRepository->checkTranslations( $fileObject , $sys_language_uid );
+			if( $checkTranslations ):
+				$file_identifier = isset($checkTranslations['translated_file'])?$checkTranslations['translated_file']:NULL;
+				if ($file_identifier && !empty( $file_identifier )){
+					$filepath = PATH_site.$file_identifier;
+					if (!empty($filepath) && is_file($filepath)){
+						$file = $filepath;
+						$fileName = basename( $file );
+					}
+				}
+			endif;
+			if(is_file($file)) {
+				$fileLen    = filesize($file);          
+				$ext        = strtolower(substr(strrchr($fileName, '.'), 1));
+				switch($ext) {
+					case 'txt':
+						$cType = 'text/plain'; 
+					break;              
+					case 'pdf':
+						$cType = 'application/pdf'; 
+					break;
+					case 'exe':
+						$cType = 'application/octet-stream';
+					break;
+					case 'zip':
+						$cType = 'application/zip';
+					break;
+					case 'doc':
+						$cType = 'application/msword';
+					break;
+					case 'xls':
+						$cType = 'application/vnd.ms-excel';
+					break;
+					case 'ppt':
+						$cType = 'application/vnd.ms-powerpoint';
+					break;
+					case 'gif':
+						$cType = 'image/gif';
+					break;
+					case 'png':
+						$cType = 'image/png';
+					break;
+					case 'jpeg':
+					case 'jpg':
+						$cType = 'image/jpg';
+					break;
+					case 'mp3':
+						$cType = 'audio/mpeg';
+					break;
+					case 'wav':
+						$cType = 'audio/x-wav';
+					break;
+					case 'mpeg':
+					case 'mpg':
+					case 'mpe':
+						$cType = 'video/mpeg';
+					break;
+					case 'mov':
+						$cType = 'video/quicktime';
+					break;
+					case 'avi':
+						$cType = 'video/x-msvideo';
+					break;
 
-                //forbidden filetypes
-                case 'inc':
-                case 'conf':
-                case 'sql':                 
-                case 'cgi':
-                case 'htaccess':
-                case 'php':
-                case 'php3':
-                case 'php4':                        
-                case 'php5':
-                exit;
+					//forbidden filetypes
+					case 'inc':
+					case 'conf':
+					case 'sql':                 
+					case 'cgi':
+					case 'htaccess':
+					case 'php':
+					case 'php3':
+					case 'php4':                        
+					case 'php5':
+					exit;
 
-                default:
-                    $cType = 'application/force-download';
-                break;
-            }
+					default:
+						$cType = 'application/force-download';
+					break;
+				}
 
-            $headers = array(
-                'Pragma'                    => 'public', 
-                'Expires'                   => 0, 
-                'Cache-Control'             => 'must-revalidate, post-check=0, pre-check=0',
-                'Cache-Control'             => 'public',
-                'Content-Description'       => 'File Transfer',
-                'Content-Type'              => $cType,
-                'Content-Disposition'       => 'attachment; filename="'. $fileName .'"',
-                'Content-Transfer-Encoding' => 'binary', 
-                'Content-Length'            => $fileLen         
-            );
-            foreach($headers as $header => $data)
-            $this->response->setHeader($header, $data); 
-            $this->response->sendHeaders();                 
-            @readfile($file);   
-        }   
+				$headers = array(
+					'Pragma'                    => 'public', 
+					'Expires'                   => 0, 
+					'Cache-Control'             => 'must-revalidate, post-check=0, pre-check=0',
+					'Cache-Control'             => 'public',
+					'Content-Description'       => 'File Transfer',
+					'Content-Type'              => $cType,
+					'Content-Disposition'       => 'attachment; filename="'. $fileName .'"',
+					'Content-Transfer-Encoding' => 'binary', 
+					'Content-Length'            => $fileLen         
+				);
+				foreach($headers as $header => $data)
+				$this->response->setHeader($header, $data); 
+				$this->response->sendHeaders();                 
+				@readfile($file);   
+			}
+		}
+		else{
+			echo "Invalid Access!";
+		}   
         exit;
     }
 
