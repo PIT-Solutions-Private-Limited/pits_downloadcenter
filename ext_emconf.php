268f62c672b6bfdf5f7bad67d436ed5a821dd11b
Hoja M A <hoja.ma@pitsolutions.com>
2015-10-01 11:59:23 +0530

    Updated Version Number to 1.0.4 from 1.0.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
-
+ 
 $EM_CONF[$_EXTKEY] = array (
 	'title' => 'Download Center',
 	'description' => 'Download Center Sponsored by TNT-Graphics AG',
